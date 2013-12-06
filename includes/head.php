0b61e9b5aeae2bd3940e63f9d1c43f6f7f00b41d
Leah de Vries <apple@leahdisease.local>
2013-12-05 19:50:29 -0500

    made php
    then deleted it bc i jumped the gun

---
+++ includes/head.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="initial-scale=1">
+ 
+  <title>Working Women - <?php echo "$pagename";?></title>
+
+  <link rel="shortcut icon" href="images/favicon.ico"> 
+  <link rel="apple-itouch-icon" href="images/favicon.ico">
+
+    <!-- Stylesheets -->
+  <link rel="stylesheet" href="styles/css/reset.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="styles/css/main.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="styles/css/print.css" type="text/css" media="print" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+ <!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>
